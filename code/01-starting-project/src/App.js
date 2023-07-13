415223dd6ad0e2626bd41fa6c82045a607542bd2
Tedis <75977150+Ted-Rose@users.noreply.github.com>
2023-07-13 19:49:16 +0000

    Defining Routes

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+// the variable has to be stored in a constant
+const router = createBrowserRouter([
+  // Define path on which this path should be active
+  // Path is following after .com like /login
+  // Also define which component should be loaded with element.
+  // For that add folder pages, but you can use whatever name
+  // you like for the folder
+  { path: "/", element: <HomePage /> },
+]);
+
 function App() {
-  return <div></div>;
+  // Pass in the createBrowserRouter values constant
+  // into RouterProvider so that for the defined path
+  // defined element would be returned
+  return <RouterProvider router={router} />;
 }
 
 export default App;
