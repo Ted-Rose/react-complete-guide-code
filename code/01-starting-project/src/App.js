9d25ddaf84d43646f0f9b523f360bbfda33ed5c0
Tedis <75977150+Ted-Rose@users.noreply.github.com>
2023-07-13 20:06:25 +0000

    Navigating between Pages with Links

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,34 +1,17 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-
-// import { createRoutesFromElements } from "react-router-dom";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-
 // the variable has to be stored in a constant
-// const router = createBrowserRouter([
-//   // Define path on which this path should be active
-//   // Path is following after .com like /login
-//   // Also define which component should be loaded with element.
-//   // For that add folder pages, but you can use whatever name
-//   // you like for the folder
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
-
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter([
+  // Define path on which this path should be active
+  // Path is following after .com like /login
+  // Also define which component should be loaded with element.
+  // For that add folder pages, but you can use whatever name
+  // you like for the folder
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 function App() {
   // Pass in the createBrowserRouter values constant
--- code/01-starting-project/src/pages/Home.js
+++ code/01-starting-project/src/pages/Home.js
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      {" "}
+      <h1>HomePage</h1>
+      {/* This redirect sends another request to backend loading all the
+      javascript code, load React, restart React application that impacts
+      site performance */}
+      <p>
+        Go to <a href="/products">products</a>
+      </p>
+      {/* Link render an anchor element, but it listens for
+      clicks on elements, preventing default for redirecting
+      and take look at route definitions to accordingly update
+      the page */}
+      <p>
+        Go to using Link <Link to="/products">products</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
