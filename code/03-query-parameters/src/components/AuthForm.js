e9b8cd23f91d6610b52799e9e2faeba437d208aa
Tedis <75977150+Ted-Rose@users.noreply.github.com>
2023-07-13 19:04:34 +0000

    Validating User Input & Outputting Validation Errors

--- code/03-query-parameters/src/components/AuthForm.js
+++ code/03-query-parameters/src/components/AuthForm.js
@@ -1,15 +1,37 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  // If some data besides redirect is returned then catch that data
+  const data = useActionData();
+  const navigation = useNavigation();
+
   const [searchParams] = useSearchParams();
-  const isLogin = searchParams.get('mode') === 'login';
+  const isLogin = searchParams.get("mode") === "login";
+  // Add helper constant to check if navigation.state is submitting
+  const isSubmitting = navigation.state === "submiting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {/* Check if data is present and data.errors object is present */}
+        {data && data.errors && (
+          <ul>
+            {/* Go trough all the values in erros object and map error messages */}
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -19,13 +41,16 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
-            {isLogin ? 'Create new user' : 'Login'}
-          </Link>
-          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
+          </Link>
+          <button disabled={isSubmitting}>
+            {/* If submitting return Submitting.. else return Save */}
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
